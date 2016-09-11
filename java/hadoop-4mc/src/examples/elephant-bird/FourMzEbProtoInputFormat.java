859726a6320c8879b93fb724cac9dd3bf394e569
Carlo Medas <carlo@domotz.com>
2016-09-11 16:55:53 +0200

    more examples on ZSTD input/output

---
+++ java/hadoop-4mc/src/examples/elephant-bird/FourMzEbProtoInputFormat.java
@@ -0,0 +1,84 @@
+import com.google.protobuf.Message;
+import com.hadoop.mapreduce.FourMcInputFormat;
+import com.twitter.elephantbird.util.HadoopCompat;
+import com.twitter.elephantbird.mapreduce.input.LzoProtobufBlockRecordReader;
+import com.twitter.elephantbird.mapreduce.io.BinaryWritable;
+import com.twitter.elephantbird.util.HadoopUtils;
+import com.twitter.elephantbird.util.TypeRef;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+/**
+ * ZSTD version.
+ * Adapter between Elephant-Bird framework and 4mc: this is able to make EB framework handle input binary protobuf
+ * files in 4mz format, seamlessly handling the input split that leverages the internal 4mz block index.
+ * Usage:
+ * where you usually set input:
+ * MultiInputFormat.setInputFormatClass(MyProtoMessage.class, job);
+ * replace with:
+ * FourMzEbProtoInputFormat.setInputFormatClass(MyProtoMessage.class, job);
+ * That's it.
+ * <p>
+ * Output format doesn't need anything, as if you want to write text output and compress it,
+ * you can just do the usual:
+ * XyzOutputFormat.setCompressOutput(job, true);
+ * XyzOutputFormat.setOutputCompressorClass(job, FourMzCodec.class);  // or e.g. FourMzMediumCodec.class
+ * <p>
+ * If instead you need to write binary protobuf as well, see FourMzEbProtoOutputFormat.
+ *
+ * @param <M> protobuf message to be read.
+ */
+public class FourMzEbProtoInputFormat<M extends Message> extends FourMzInputFormat<LongWritable, BinaryWritable<M>> {
+
+    private TypeRef<M> typeRef;
+    private static String CLASS_CONF_KEY = "elephantbird.class.for.FourMzEbProtoInputFormat";
+
+    public static void setInputFormatClass(Class<?> clazz, Job job) {
+        job.setInputFormatClass(FourMzEbProtoInputFormat.class);
+        setClassConf(clazz, job.getConfiguration());
+    }
+
+    public FourMzEbProtoInputFormat() {
+    }
+
+    public FourMzEbProtoInputFormat(TypeRef<M> tr) {
+        this.typeRef = tr;
+    }
+
+    @Override
+    public RecordReader<LongWritable, BinaryWritable<M>> createRecordReader(InputSplit split, TaskAttemptContext taskAttempt) {
+        Configuration conf = taskAttempt.getConfiguration();
+        if (typeRef == null) {
+            setTypeRef(conf);
+        }
+        return new LzoProtobufBlockRecordReader(typeRef);
+    }
+
+    public static void setClassConf(Class<?> clazz, Configuration conf) {
+        HadoopUtils.setClassConf(conf, CLASS_CONF_KEY, clazz);
+    }
+
+    private void setTypeRef(Configuration conf) {
+        String className = conf.get(CLASS_CONF_KEY);
+
+        if (className == null) {
+            throw new RuntimeException(CLASS_CONF_KEY + " is not set");
+        }
+
+        Class<?> clazz = null;
+        try {
+            clazz = conf.getClassByName(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("failed to instantiate class '" + className + "'", e);
+        }
+
+        typeRef = new TypeRef<M>(clazz) {
+        };
+    }
+
+
+}
